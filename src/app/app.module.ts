6cff8f8e692dcad3146ad0748dc3d4511fe070e9
fatemeh133 <121554370+fatemeh133@users.noreply.github.com>
2024-04-22 12:22:50 +0330

    Todo generate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { IndexComponent } from './index/index.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    IndexComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
